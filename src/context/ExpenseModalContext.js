b4975ca29a3e99f2b52cc23a72a11486265190b8
jshan98 <jshanahan@ggc.edu>
2026-04-06 16:59:25 -0400

    Setup ExpenseModalContext by importing required references and creating context

---
+++ src/context/ExpenseModalContext.js
@@ -0,0 +1,6 @@
+// ExpenseModalContext.js
+
+import { createContext, useContext, useState } from "react";
+import ExpenseModal from '../components/ExpenseModal';
+
+const ExpenseModalContext = createContext();
