7b8ed2caf1369b04744dec9d702e8d4c46064312
jshan98 <jshanahan@ggc.edu>
2026-04-06 17:16:39 -0400

    defined state variables and created functions to handle the showing and closing of the modal

--- src/context/ExpenseModalContext.js
+++ src/context/ExpenseModalContext.js
@@ -4,3 +4,53 @@ import { createContext, useContext, useState } from "react";
 import ExpenseModal from '../components/ExpenseModal';
 
 const ExpenseModalContext = createContext();
+
+const ExpenseModalProvider = ({ children }) => {
+    const [showModal, setShowModal] = useState(false);
+    const [modalMode, setModalMode] = useState(null);
+    const [modalData, setModalData] = useState({
+        "_id":"",
+        "description":"",
+        "amount":0,
+        "date":"",
+        "categoryName":"",
+    }); 
+
+    // Sets showModal to true, sets modalMode to mode parameter, and sets modalData to data parameter 
+    const handleShow = (mode, data) => {
+        setShowModal(true);
+        setModalMode(mode);
+        setModalData(data);
+    }
+
+    // Reverts showModal, modalMode, and modalData to their base initializations 
+    const handleClose = () => {
+        setShowModal(false);
+        setModalMode(null);
+        setModalData({
+            "_id":"",
+            "description":"",
+            "amount":0,
+            "date":"",
+            "categoryName":"",
+        });
+    }
+
+    return (
+        <ExpenseModalContext.Provider
+            value={{
+                showModal,
+                modalMode,
+                modalData,
+                handleShow,
+                handleClose
+            }} >
+            {children} {showModal && <ExpenseModal />} {/* If showModal == true render the ExpenseModal component */}
+        </ExpenseModalContext.Provider>
+    );
+};
+
+// Custom hook to use ExpenseModalContext in other components
+export const useExpenseModalContext = () => {
+    return useContext(ExpenseModalContext);
+};
