b585d349b7d961e5481192f25efd24004ff5a4c6
jshan98 <jshanahan@ggc.edu>
2026-04-06 16:42:08 -0400

    Defined & initialized state variables and created custom hook to use AppContext in other components

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,6 +1,31 @@
 // AppContext.js
 
 import { createContext, useContext, useState, useEffect } from "react";
-import { expenseData, expenseSummaryData, expenseCategories } from 'data.js';
+import { expenseData, expenseSummaryData as summaryData, expenseCategories as categoriesData } from 'data.js';
 
+// Create a new context for the app
 const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+    const [month, setMonth] = useState("2020-01");
+    const [expenseSummaryData, setExpenseSummaryData] = useState(null);
+    const [expenseDetailsData, setExpenseDetailsData] = useState(null);
+    const [totalExpenses, setTotalExpenses] = useState(0);
+    const [expenseCategories, setExpenseCategories] = useState(null);
+    const [expenseIdToBeDeleted, setexpenseIdToBeDeleted] = useState(null);
+
+    const fetchExpenseCategories = async () => {
+        setExpenseCategories(categoriesData.categories);
+    }
+
+    const fetchExpenseData = async () => {
+        setExpenseSummaryData(summaryData);
+        setTotalExpenses(expenseData.totalExpenses);
+        setExpenseDetailsData(expenseData.expenses);
+    }
+
+};
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+};
