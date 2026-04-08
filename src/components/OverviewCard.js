342f0e6a6b27a0d3ac20abea9379ec2510ed9fba
jshan98 <jshanahan@ggc.edu>
2026-04-08 19:14:09 -0400

    Created and implemented OverviewCard component

--- src/components/OverviewCard.js
+++ src/components/OverviewCard.js
@@ -6,10 +6,26 @@ import { Card } from "react-bootstrap";
  * @param {*} imageName 
  * @returns String ("./images/imageName")
  */
-function getImageUrl(imageName){
+export function getImageUrl(imageName){
     return ("./images/" + imageName);
 }
 
+function OverviewCard(props) {
+    return(
+        <Card>
+            <Card.Body>
+                <div className="d-flex align-items-center">
+                    <div className="mr-3">
+                        <Card.Img variant="top" src={getImageUrl(props.cardIcon)} alt={props.cardText}/>
+                    </div>
+                    <div>
+                        <Card.Text >{props.cardText}</Card.Text>
+                        <Card.Title >${props.cardTitle}</Card.Title>
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
+    );
+}
 
-
-export default {getImageUrl}
+export default OverviewCard;
