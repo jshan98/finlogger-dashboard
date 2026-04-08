b8dc40ffe8a11257782a87c8ff31f12b2584791c
jshan98 <jshanahan@ggc.edu>
2026-04-08 18:01:43 -0400

    Made functional components for rendering the app and its' content. Also exported provider functions within AppContext and ExpenseModalContext respectively

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Overview from './components/Overview';
+import ExpenseSummary from './components/ExpenseSummary';
+import ExpenseDetails from './components/ExpenseDetails';
+import { ExpenseModalProvider } from './context/ExpenseModalContext';
+import { Container, Row } from 'react-bootstrap';
+import { user } from './data';
+import { AppProvider, useAppContext } from './context/AppContext';
+
+function AppContent() {
+  const {month, expenseSummaryData, expenseDetailsData, totalExpenses, setMonth} = useAppContext();
+
+  const handleMonthChange = (event) => {
+    setMonth(event.target.value);
+  };
+
+  return(
+    <div className='App'>
+      <Header />
+      <ExpenseModalProvider>
+        <Container fluid='lg'>
+          <Overview />
+          <Row className='tbl-container'>
+            <ExpenseSummary />
+            <ExpenseDetails />
+          </Row>
+        </Container >
+      </ExpenseModalProvider>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider >
+      <AppContent />
+    </AppProvider>
   );
 }
 
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -6,7 +6,7 @@ import { expenseData, expenseSummaryData as summaryData, expenseCategories as ca
 // Create a new context for the app
 const AppContext = createContext();
 
-const AppProvider = ({ children }) => {
+export const AppProvider = ({ children }) => {
     const [month, setMonth] = useState("2020-01");
     const [expenseSummaryData, setExpenseSummaryData] = useState(null);
     const [expenseDetailsData, setExpenseDetailsData] = useState(null);
