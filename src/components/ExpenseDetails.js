d6a952b14a9aacb2b6b4873afc3980f17882c740
jshan98 <jshanahan@ggc.edu>
2026-04-09 19:48:00 -0400

    Added comments to updateDateFormat for documentation purposes

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -3,10 +3,16 @@ import DeleteConfirmationModal from './DeleteConfirmationModal';
 import { useExpenseModalContext } from "../context/ExpenseModalContext";
 import { useState } from "react";
 
-function updateDateFormat(){
+/**
+ * Function: updateDateFormat
+ * Description: reformats the parameter to the correcr format.
+ * @param {*} date 
+ * @returns formatted date (MM/DD/DayOfTheWeek)
+ */
+function updateDateFormat(date){
     const d1 = new Date(date);
     const month = d1.toLocaleString("en-us", {month:"short"});
     const day = d1.getDate();
     const dayOfWeek = d1.toLocaleString("en-us", {weekday:"short"});
-    return ('${month}${day}${dayOfWeek}');
+    return ('${month}${day},${dayOfWeek}');
 }
