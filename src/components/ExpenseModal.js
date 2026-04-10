257b7e2729a337a2102dc6f308110f4051b5e772
jshan98 <jshanahan@ggc.edu>
2026-04-10 10:09:48 -0400

    Imported required references for ExpenseModal component

---
+++ src/components/ExpenseModal.js
@@ -0,0 +1,4 @@
+import { expenseCategories } from "../data";
+import { useExpenseModalContext } from "../context/ExpenseModalContext";
+import { Modal, Button, Form } from 'react-bootstrap';
+import { useState } from "react";
