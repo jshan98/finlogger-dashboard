188500eddfe6651ce62c9d3e7431eac8c30f99a4
jshan98 <jshanahan@ggc.edu>
2026-04-10 10:17:29 -0400

    Set state variable for validated and used inherited states from ExpenseModalContext. Also iterated through expense categories to populate category options

--- src/components/ExpenseModal.js
+++ src/components/ExpenseModal.js
@@ -2,3 +2,16 @@ import { expenseCategories } from "../data";
 import { useExpenseModalContext } from "../context/ExpenseModalContext";
 import { Modal, Button, Form } from 'react-bootstrap';
 import { useState } from "react";
+
+function ExpenseModal() {
+    const {showModal, modalMode, modalData, handleClose} = useExpenseModalContext();
+    const {validated, setValidated} = useState(false);
+
+    const categories = expenseCategories.categories.map((item) => {
+        return (
+            <option value={item} key={item}>
+                {item}
+            </option>
+        );
+    });
+}
